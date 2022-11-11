0791363c15fca77f83dbc14452f4cf6ca60ac15c
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-11-11 12:57:09 +0100

    Add WAVES mode

--- master/include/clock_config.h
+++ master/include/clock_config.h
@@ -19,6 +19,8 @@ enum clock_modes
 {
   LAZY,
   FUN,
+  //CENTRIC,
+  WAVES,
   OFF
 };
 
--- master/src/main.cpp
+++ master/src/main.cpp
@@ -11,24 +11,16 @@
 #include "clock_config.h"
 #include "ntp.h"
 
-const t_full_clock d_stop = {digit_null, digit_null, digit_null, digit_null};
-
 int last_hour = -1;
 int last_minute = -1;
 bool is_stopped = false;
 
-// Interfaccia che replica 
+void stop();
+void set_time(int mode);
+void set_lazy();
+void set_fun();
+void set_waves();
 
-void stop()
-{
-  if(!is_stopped)
-  {
-    is_stopped = true;
-    last_hour = -1;
-    last_minute = -1;
-    set_clock(d_stop);
-  }
-}
 
 void setup() {
   Serial.begin(115200);
@@ -38,15 +30,14 @@ void setup() {
 
   Wire.begin();
   pinMode(LED_BUILTIN, OUTPUT);
-  set_mode(LAZY);
 
   if(get_connection_mode() == HOTSPOT)
-    wifi_create_AP();
+    wifi_create_AP("ClockClock 24", "clockclock24");
   else
   {
-    if( !wifi_connect(get_ssid(), get_password()) ){
+    if( !wifi_connect(get_ssid(), get_password(), "clockclock24") ){
       set_connection_mode(HOTSPOT);
-      wifi_create_AP();
+      wifi_create_AP("ClockClock 24", "clockclock24");
     }
   }
   server_start();
@@ -75,30 +66,92 @@ void loop() {
     setSyncProvider(get_NTP_time);
   }
 
-  switch(get_clock_mode())
-  {
-    case LAZY:
-    case FUN:
-      {
-        int day_week = (weekday() + 5) % 7;
-        if(get_sleep_time(day_week, hour()))
-          stop();
-        else if(hour() != last_hour || minute() != last_minute)
-        {
-          is_stopped = false;
-          last_hour = hour();
-          last_minute = minute();
-          digitalWrite(LED_BUILTIN, LOW);
-          set_clock_time(last_hour, last_minute);
-          digitalWrite(LED_BUILTIN, HIGH);
-        }
-      }
-      break;
-    case OFF:
-      stop();
-      break;
-  }
+  get_clock_mode() != OFF ? set_time(get_clock_mode()) : stop();
 
   update_MDNS();
   handle_webclient();
+}
+
+void _delay(int value)
+{
+  for (int i = 0; i <value/100; i++)
+  {
+    update_MDNS();
+    handle_webclient();
+    delay(value/100);
+  }
+}
+
+void set_time(int mode)
+{
+  //mode == LAZY ? set_direction(MIN_DISTANCE);
+  int day_week = (weekday() + 5) % 7;
+  if(get_sleep_time(day_week, hour()))
+    stop();
+  else if(hour() != last_hour || minute() != last_minute)
+  {
+    is_stopped = false;
+    last_hour = hour();
+    last_minute = minute();
+    switch(mode)
+    {
+      case LAZY:
+        set_lazy();
+        break;
+      case FUN:
+        set_fun();
+        break;
+      case WAVES:
+        set_waves();
+        break;
+    }
+  }
+}
+
+void set_lazy()
+{
+  set_speed(200);
+  set_acceleration(100);
+  set_direction(MIN_DISTANCE);
+  set_clock_time(last_hour, last_minute);
+}
+
+void set_fun()
+{
+  set_speed(400);
+  set_acceleration(150);
+  set_direction(CLOCKWISE2);
+  set_clock_time(last_hour, last_minute);
+}
+
+void set_waves()
+{
+  set_speed(800);
+  set_acceleration(150);
+  set_direction(MIN_DISTANCE);
+  set_clock(d_IIII);
+  _delay(9000);
+  set_speed(400);
+  set_acceleration(100);
+  set_direction(CLOCKWISE2);
+  t_full_clock clock = get_clock_state_from_time(last_hour, last_minute);
+  for (int i = 0; i <8; i++)
+  {
+    set_half_digit(i, clock.digit[i/2].halfs[i%2]);
+    delay(400);
+  }
+}
+
+void stop()
+{
+  if(!is_stopped)
+  {
+    is_stopped = true;
+    last_hour = -1;
+    last_minute = -1;
+    set_direction(MIN_DISTANCE);
+    set_speed(200);
+    set_acceleration(100);
+    set_clock(d_stop);
+  }
 }
